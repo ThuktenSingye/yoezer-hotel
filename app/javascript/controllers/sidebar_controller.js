37eba683becd6c13b832c4adf8e887659968b1ce
ThuktenSingye <thuktensingye2163@gmail.com>
2025-01-09 17:27:32 +0600

    Responsive Admin Sidebar

---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+import { toggle} from "el-transition";
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+  static  targets = ["menu", "sidebar", "backdrop"]
+  toggle() {
+    console.log("This is when click")
+    toggle(this.menuTarget);
+  }
+  toggleSidebar(event) {
+    const sidebarToggleable = document.getElementById("sidebar-toggle")
+    sidebarToggleable.classList.toggle('hidden')
+
+    this.sidebarTarget.classList.toggle("-translate-x-full");
+    this.backdropTarget.classList.toggle('hidden')
+  }
+}
