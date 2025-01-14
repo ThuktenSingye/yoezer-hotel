ea0eb59f68df3fec236d9657ed97ef5f694b10c0
ThuktenSingye <thuktensingye2163@gmail.com>
2025-01-14 12:13:35 +0600

    Addressed PR review comments

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,16 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
-import { toggle} from "el-transition";
-// Connects to data-controller="dropdown"
-export default class extends Controller {
-  static  targets = ["menu", "sidebar", "backdrop"]
-  toggle() {
-    toggle(this.menuTarget);
-  }
-  toggleSidebar(event) {
-    const sidebarToggleable = document.getElementById("sidebar-toggle")
-    sidebarToggleable.classList.toggle('hidden')
+  import { Controller } from "@hotwired/stimulus"
+  import { toggle} from "el-transition";
+  // Connects to data-controller="dropdown"
 
-    this.sidebarTarget.classList.toggle("-translate-x-full");
-    this.backdropTarget.classList.toggle('hidden')
+  export default class extends Controller {
+
+    static  targets = ["roomMenu", "hotelMenu","sidebar", "backdrop", "sidebar_toggle"]
+
+    toggleRoomMenu(){
+      toggle(this.roomMenuTarget)
+    }
+
+    toggleHotelMenu(){
+      toggle(this.hotelMenuTarget)
+    }
+
+    toggleSidebar(event) {
+      this.sidebar_toggleTarget.classList.toggle('hidden')
+      this.sidebarTarget.classList.toggle("-translate-x-full");
+      this.backdropTarget.classList.toggle('hidden')
+    }
+
   }
-}
