aa6d7fae1cee29653a9c9e09b4e8881478b497e6
ThuktenSingye <thuktensingye2163@gmail.com>
2025-01-27 19:09:30 +0600

    Updated Spec

--- spec/models/room_bed_type_spec.rb
+++ spec/models/room_bed_type_spec.rb
@@ -5,8 +5,6 @@ require 'rails_helper'
 RSpec.describe RoomBedType, type: :model do
   context 'when validating attributes' do
     it { is_expected.to validate_presence_of :num_of_bed }
-    it { is_expected.to validate_presence_of :room_id }
-    it { is_expected.to validate_presence_of :bed_type_id }
     it { is_expected.to validate_numericality_of(:num_of_bed).only_integer }
   end
 
