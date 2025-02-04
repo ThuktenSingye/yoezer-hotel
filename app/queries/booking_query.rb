0a64bb8bdbe1ec50c4148d181dbc642611d45a4c
ThuktenSingye <thuktensingye2163@gmail.com>
2025-02-04 19:21:03 +0600

    Added filtering by checkin and checkout date

--- app/queries/booking_query.rb
+++ app/queries/booking_query.rb
@@ -5,11 +5,31 @@ class BookingQuery < BaseQuery
   def call
     bookings = @hotel.bookings
     bookings = filter_by_status(bookings) if @params[:status].present?
+    bookings = filter_by_checkin_date(bookings) if @params[:checkin_date].present?
+    bookings = filter_by_checkout_date(bookings) if @params[:checkout_date].present?
     bookings = search_booking(bookings) if @params[:query].present?
     ordered_records(bookings)
   end
 
   private
+
+  def filter_by_checkin_date(bookings)
+    if @params[:checkin_date].present?
+      checkin_date = Date.parse(@params[:checkin_date])
+      bookings = bookings.where(checkin_date: checkin_date)
+    end
+
+    bookings
+  end
+
+  def filter_by_checkout_date(bookings)
+    if @params[:checkout_date].present?
+      checkout_date = Date.parse(@params[:checkout_date])
+      bookings = bookings.where(checkout_date: checkout_date)
+    end
+
+    bookings
+  end
 
   def filter_by_status(bookings)
     if Room.statuses.key?(@params[:status])
@@ -26,19 +46,6 @@ class BookingQuery < BaseQuery
       'guests.first_name ILIKE :query OR guests.last_name ILIKE :query OR
      rooms.room_number ILIKE :query OR room_categories.name ILIKE :query',
       query: query
-    )
-  end
-
-  def ordered_records(bookings)
-    today = Time.zone.today
-    bookings.order(
-      Arel.sql(
-        "CASE
-          WHEN checkin_date = ? THEN 0
-          WHEN checkout_date = ? THEN 1
-          ELSE 2
-        END", today, today
-      )
     )
   end
 end
