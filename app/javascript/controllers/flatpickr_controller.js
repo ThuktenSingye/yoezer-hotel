df855ccaa76426767ac9e65549c818a7aacadd7b
ThuktenSingye <thuktensingye2163@gmail.com>
2025-02-11 00:20:14 +0600

    Updated Flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,9 +4,13 @@ import flatpickr from "flatpickr";
 
 export default class extends Controller {
   connect() {
+    const minDate = this.data.get("min-date");
+    const maxDate = this.data.get("max-date");
     flatpickr(this.element, {
       dateFormat: "Y-m-d",
-      disableMobile: true
+      disableMobile: true,
+      minDate: minDate,
+      maxDate: maxDate
     });
   }
 }
