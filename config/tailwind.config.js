f8a637bdf6e30169b4b059e1be32b978a3938840
ThuktenSingye <thuktensingye2163@gmail.com>
2025-02-11 23:17:30 +0600

    Updated Styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         'error': '#FF543E',
         'rating': '#FFCE31',
         'white': '#FFFFFF',
-        'tertiary': '#153448',
+        'tertiary': '#393E46',
         'text-color': '#1C1C1C'
       },
       fontFamily: {
