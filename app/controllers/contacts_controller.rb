9a375002c89a1c38717c6db44354a2df9fc4642b
ThuktenSingye <thuktensingye2163@gmail.com>
2025-02-12 15:54:45 +0600

    Rubocop Correction

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Contact us page controller
 class ContactsController < HomeController
   def index
     @feedback = @hotel.feedbacks.new
--- app/helpers/hotels_rating_helper.rb
+++ app/helpers/hotels_rating_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Rating helper module
 module HotelsRatingHelper
 end
