0fea65b1888e513b0db074de40af20bca6047528
ThuktenSingye <thuktensingye2163@gmail.com>
2025-02-13 16:06:52 +0600

    Updated Room Spec

--- app/queries/room_query.rb
+++ app/queries/room_query.rb
@@ -6,7 +6,7 @@ class RoomQuery < BaseQuery
     rooms = @hotel.rooms
     rooms = filter_by_status(rooms) if @params[:status].present?
     rooms = filter_by_category(rooms) if @params[:room_category_id].present?
-    rooms = filter_by_query(rooms) if @params[:query].present?
+    rooms = filter_by_price(rooms) if @params[:query].present?
 
     ordered_records(rooms.includes(:room_ratings))
   end
@@ -25,7 +25,7 @@ class RoomQuery < BaseQuery
     rooms.where(room_category_id: @params[:room_category_id])
   end
 
-  def filter_by_query(rooms)
+  def filter_by_price(rooms)
     query = @params[:query].strip
 
     if query.match?(/^\d+-\d+$/)
